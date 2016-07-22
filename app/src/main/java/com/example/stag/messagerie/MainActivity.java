9f2d45a4e6dfd0ef5ba223efb05087a5dc87e277
leyriel <maloba.eric@gmail.com>
2016-07-22 12:05:12 +0200

    home views and button

--- app/src/main/java/com/example/stag/messagerie/MainActivity.java
+++ app/src/main/java/com/example/stag/messagerie/MainActivity.java
@@ -2,12 +2,34 @@ package com.example.stag.messagerie;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button startConversButton;
+    private Button registerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startConversButton = (Button)findViewById(R.id.startButton);
+        registerButton     = (Button)findViewById(R.id.registerButton);
+
+        startConversButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
